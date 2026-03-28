a0527db927db579c04688498c7ace938bac92879
Engineer <engineer.fractal@rajeshgo.li>
2026-03-28 13:57:40 -0700

    E: artifact server + domain rename

--- android/app/src/main/java/com/rajesh/officeclimate/util/Constants.kt
+++ android/app/src/main/java/com/rajesh/officeclimate/util/Constants.kt
@@ -7,7 +7,7 @@ object Thresholds {
 }
 
 object Defaults {
-    const val SERVER_URL = "https://climate.rajeshgo.li"
+    const val SERVER_URL = "https://office.rajeshgo.li"
     const val POLL_INTERVAL_MS = 5000L
     const val WS_RECONNECT_BASE_MS = 3000L
     const val WS_RECONNECT_MAX_MS = 30000L
